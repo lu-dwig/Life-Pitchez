adf57336d57bed9f8545827303d59bf7517f44c0
ludwig <murimiludwig@gmail.com>
2022-05-11 15:07:59 +0300

    this will modify the manage.py file

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
+    MAIL_PORT = 587
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
